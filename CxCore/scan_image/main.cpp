a8a559eb6371c4a4fca9f2b19215bd26f88e8050
eager7 <panchagntao@gmail.com>
2016-10-11 17:43:09 +0800

    add core func

--- CxCore/scan_image/main.cpp
+++ CxCore/scan_image/main.cpp
@@ -47,6 +47,46 @@ Mat& ReduceImage(Mat &I, const uchar *const table)
 	return I;
 }
 
+Mat& ReduceImageIterator(Mat &I, const uchar *const table)
+{
+	CV_Assert(I.depth() != sizeof(uchar));
+
+	switch (I.channels()){
+		case 1:{
+			MatIterator_<uchar> it, end;
+			for (it = I.begin<uchar>(), end = I.end<uchar>(); it != end ; ++it) {
+				*it = table[*it];
+			}
+		}
+			break;
+		case 3:{
+			MatIterator_<Vec3b> it, end;
+			for (it = I.begin<Vec3b>(), end = I.end<Vec3b>(); it != end ; ++it) {
+				(*it)[0] = table[(*it)[0]];
+				(*it)[1] = table[(*it)[1]];
+				(*it)[2] = table[(*it)[2]];
+			}
+		}
+			break;
+		default:
+			break;
+	}
+	return I;
+}
+
+Mat& ReduceImageCore(Mat &I, const uchar *const table)
+{
+	Mat &J = I;
+	Mat lookUpTable(1, 256, CV_8U);
+	uchar *p = lookUpTable.data;
+	for (int i = 0; i < 256; ++i) {
+		p[i] = table[i];
+	}
+
+	LUT(I, lookUpTable, J);
+	return J;
+}
+
 int main()
 {
 	Mat Src;
@@ -56,12 +96,24 @@ int main()
 	waitKey();
 
 	make_table(50);
+
 	time_start();
 	Mat &R = ReduceImage(Src, table);
 	time_done();
 	imshow("test", R);
 	waitKey();
 
-	return 0;
+	time_start();
+	Mat &RI = ReduceImageIterator(Src, table);
+	time_done();
+	imshow("test", RI);
+	waitKey();
 
+	time_start();
+	Mat &RC = ReduceImageCore(Src, table);
+	time_done();
+	imshow("test", RC);
+	waitKey();
+
+	return 0;
 }
